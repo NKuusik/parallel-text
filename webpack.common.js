84ea374d1a060f95bf44fd804c068db0a0b202bf
NKuusik <normankuusik@gmail.com>
2018-07-21 21:00:12 +0300

    1. In the .scss file added some nesting and variables. 2. Installed the mini-css-extract-plugin so that styles could be references from an external style sheet. 3. Deleted index.js which was previously used to add an internal style sheet to index.html file.

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,29 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/parallel_text.js',
   output: {
-    filename: 'index.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins:  [
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+  ],
    module: {
      rules: [
        {
         test: /\.scss$/,
          use: [
-           'style-loader',
-           'css-loader',
-           'sass-loader'
+           {
+             loader: MiniCssExtractPlugin.loader,
+             options: {
+               publicPath: '../'
+             }
+          },
+           'css-loader', 'sass-loader'
          ]
        }
      ]
