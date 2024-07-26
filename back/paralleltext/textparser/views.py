290b21507d9fbd49beb5caae538ec811e7ce9088
Norman Kuusik <normankuusik@gmail.com>
2024-07-26 13:37:00 +0300

    Use UploadFileForm for form validation.

--- back/paralleltext/textparser/views.py
+++ back/paralleltext/textparser/views.py
@@ -12,17 +12,19 @@ def index(request):
             "first_file": {},
             "second_file": {}
                      }
-        for key in request.FILES:
-            file = request.FILES[key]
-            file_dict[key] = {
-                "title": file.name,
-                "lines": []
-            }
-            lines = file.readlines()
-            for line in lines:
-                decoded_line = line.decode("utf-8")
-                stripped_line = decoded_line.rstrip()
-                file_dict[key]['lines'].append(stripped_line)      
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            for key in request.FILES:
+                file = request.FILES[key]
+                file_dict[key] = {
+                    "title": file.name,
+                    "lines": []
+                }
+                lines = file.readlines()
+                for line in lines:
+                    decoded_line = line.decode("utf-8")
+                    stripped_line = decoded_line.rstrip()
+                    file_dict[key]['lines'].append(stripped_line)      
         return JsonResponse(file_dict)
     else:
         return HttpResponse("Boo")
