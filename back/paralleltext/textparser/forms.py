4bda028174aef4604ade59cc50049410a63594bb
Norman Kuusik <normankuusik@gmail.com>
2024-07-26 12:19:26 +0300

    POST request on /text returns a JSON file with names of uploaded files.

--- back/paralleltext/textparser/forms.py
+++ back/paralleltext/textparser/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50)
-    file = forms.FileField()
+    first_file = forms.FileField()
+    second_file = forms.FileField()
--- back/paralleltext/textparser/views.py
+++ back/paralleltext/textparser/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import UploadFileForm
+import json
 
 @csrf_exempt
 def index(request):
 
     if request.method == 'POST':
-        return(HttpResponse(request.FILES))
+        file_dict = {
+            "first_file": {},
+            "second_file": {}
+                     }
+        for key in request.FILES:
+            file = request.FILES[key]
+            file_dict[key] = {
+                "title": file.name
+            }
+            lines = file.readlines()
+            for line in lines:
+                pass            
+        return JsonResponse(file_dict)
     else:
         return HttpResponse("Boo")
