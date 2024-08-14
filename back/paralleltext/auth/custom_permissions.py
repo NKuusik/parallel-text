a6c47b6782a01961fe40903948869ea6ebcfd8e0
Norman Kuusik <normankuusik@gmail.com>
2024-08-14 15:57:54 +0300

    Cleanup.

--- back/paralleltext/auth/custom_permissions.py
+++ back/paralleltext/auth/custom_permissions.py
@@ -1,28 +1,22 @@
 from rest_framework import permissions
 from django.conf import settings
-from pprint import pprint
 
 # Permission
 class WhitelistPermission(permissions.BasePermission):
     """
     Global permission check for whitelisted domain names.
     """
-
     def has_permission(self, request, view):
-        pprint(request.META)
         has_permission = False
         whitelisted_origins = settings.WHITELISTED_ORIGINS
         if settings.DEBUG is True:
              has_permission = True
         else: 
-            
             domain = None
             if 'HTTP_ORIGIN' in request.META:
                 domain = request.META['HTTP_ORIGIN']
             elif 'HTTP_REFERER' in request.META:
                 domain = request.META['HTTP_REFERER']
-            pprint(domain)
             
             has_permission = domain in whitelisted_origins
-        pprint(whitelisted_origins)
         return has_permission
