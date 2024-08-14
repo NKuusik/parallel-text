5d72bce422ca43124ca53a59738f7e327ca7ea3f
Norman Kuusik <normankuusik@gmail.com>
2024-08-14 14:36:37 +0300

    Revert use of whitelisted origins.

--- back/paralleltext/auth/custom_permissions.py
+++ back/paralleltext/auth/custom_permissions.py
@@ -8,10 +8,11 @@ class WhitelistPermission(permissions.BasePermission):
     """
 
     def has_permission(self, request, view):
-        domain = request.META['REMOTE_HOST']
+        #domain = request.META['REMOTE_HOST']
         print(request)
-        print(domain)
-        remote_address = request.META['REMOTE_ADDR']
-        print(remote_address)
-        whitelisted_origins = settings.WHITELISTED_ORIGINS
-        return domain in whitelisted_origins or remote_address in whitelisted_origins
+        #print(domain)
+        #remote_address = request.META['REMOTE_ADDR']
+        #print(remote_address)
+        #whitelisted_origins = settings.WHITELISTED_ORIGINS
+        #return domain in whitelisted_origins or remote_address in whitelisted_origins
+        return True
