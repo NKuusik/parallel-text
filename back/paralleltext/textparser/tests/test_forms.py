06b2381314f6ce9d52a12d33cb492f9c25bd6743
Norman Kuusik <normankuusik@gmail.com>
2024-08-15 13:22:06 +0300

    Add additional test cases for UploadFileFormTestCase

--- back/paralleltext/textparser/tests/test_forms.py
+++ back/paralleltext/textparser/tests/test_forms.py
@@ -3,8 +3,27 @@ from ..forms import UploadFileForm
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 class UploadFileFormTestCase(TestCase):
+
     def test_form_is_valid(self):
-        file1 = SimpleUploadedFile('file1.txt', b"I am a fake file 1", content_type="text/plain")
-        file2 = SimpleUploadedFile('file2.txt', b"I am a fake file 2", content_type="text/plain")
+        file1 = SimpleUploadedFile('file1.txt', b"I am a test file 1", content_type="text/plain")
+        file2 = SimpleUploadedFile('file2.txt', b"I am a test file 2", content_type="text/plain")
         form = UploadFileForm(files={'first_file': file1, 'second_file': file2})
         self.assertTrue(form.is_valid())
+
+    def test_form_incorrect_file_extension(self):
+        file1 = SimpleUploadedFile('file1.exe', b"I am a test file 1", content_type="text/plain")
+        file2 = SimpleUploadedFile('file2.txt', b"I am a test file 2", content_type="text/plain")
+        form = UploadFileForm(files={'first_file': file1, 'second_file': file2})
+        self.assertFalse(form.is_valid())
+    
+    def test_form_incorrect_JSON(self):
+        file1 = SimpleUploadedFile('file1.txt', b'{"jsonObject": {"isJSON": true}}', content_type="text/plain")
+        file2 = SimpleUploadedFile('file2.txt', b"I am a test file 2", content_type="application/x-custom")
+        form = UploadFileForm(files={'first_file': file1, 'second_file': file2})
+        self.assertFalse(form.is_valid())
+
+    def test_form_incorrect_bash(self):
+        file1 = SimpleUploadedFile('file1.txt', b'I am a test file 1', content_type="text/plain")
+        file2 = SimpleUploadedFile('file2.txt', b'#!/bin/bash\necho "Hello World"', content_type="application/x-custom")
+        form = UploadFileForm(files={'first_file': file1, 'second_file': file2})
+        self.assertFalse(form.is_valid())
--- back/paralleltext/textparser/validators.py
+++ back/paralleltext/textparser/validators.py
@@ -4,13 +4,14 @@ import magic
 
 def validate_text_file(file_buffer):
 
-
     # Validating a copy of the buffer ensures the whole file is retained
     validation_buffer = copy.deepcopy(file_buffer)
     magic_app = magic.Magic()
     mime_type = magic_app.from_buffer(validation_buffer.read(1024))
-    valid_mime_types = ['ASCII text', 'Unicode text']
-    if any(valid_type in mime_type for valid_type in valid_mime_types):
+    allowed_mime_type_elements = ['ASCII text', 'Unicode text']
+    forbidden_mime_type_elements = ['executable', 'shell', 'script']
+    if any(valid_type in mime_type for valid_type in allowed_mime_type_elements) \
+        and not(any(forbidden_type in mime_type for forbidden_type in forbidden_mime_type_elements)):
         return file_buffer
     else:
         raise ValidationError("Invalid file type")
