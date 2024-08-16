0c82d6668b0d59194b5fb498a1a6c4b119200bd4
Norman Kuusik <normankuusik@gmail.com>
2024-08-16 14:08:53 +0300

    Change functionality of Text.post() so that it accepts optional delim parameter and defaults to newline if none are provided. Add addtional logic to maintain delimiter in string if it is not newline.

--- back/paralleltext/textparser/views.py
+++ back/paralleltext/textparser/views.py
@@ -28,6 +28,9 @@ class Text(APIView):
                      }
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
+            separator = request.GET.get('delim')
+            if separator == 'newline' or separator is None:
+                separator = '\n'
             for key in request.FILES:
                 file = request.FILES[key]
 
@@ -35,12 +38,20 @@ class Text(APIView):
                     "title": file.name,
                     "lines": []
                 }
+                text = file.read().decode("utf-8")
+                lines = text.split(separator)
+                stripped_lines = []
+                # Restore the separator to the end of if it is not newline
+                for x in range(len(lines)):
+                    current_line = lines[x]
+                    if current_line != '':
+                        if x < len(lines) - 1:
+                            current_line += separator
+                        current_line = current_line.strip()
+                        current_line = current_line.replace('\n', ' ')
+                        stripped_lines.append(current_line)
+                file_dict[key]['lines'] = stripped_lines
 
-                lines = file.readlines()
-                for line in lines:
-                    decoded_line = line.decode("utf-8")
-                    stripped_line = decoded_line.rstrip()
-                    file_dict[key]['lines'].append(stripped_line)
             return JsonResponse(file_dict)
         error_message = "Invalid data submitted"
         logger.warning("%s:%s", error_message, list((form.errors.values())))
