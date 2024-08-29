74a895261bef5385ba9e7a948765ed1756cecd2c
Norman Kuusik <normankuusik@gmail.com>
2024-08-29 14:20:11 +0300

    Add proper styling for compared text.

--- front/src/components/TextDisplayContainer.vue
+++ front/src/components/TextDisplayContainer.vue
@@ -15,23 +15,13 @@ const isNull = (value) => {
 		<div v-for="text in displayedTextArray['lines']" v-bind:key="text" class='col-6 mb-4 displayed-texts'>
 			{{ text }}
 		</div>
-		<div class='col-6 mb-4'>
+		<div v-for="i in 2" class='col-6 mb-4 displayed-texts'>
 				<span v-for="allComparisons in displayedTextArray['comparison']" v-bind:key="allComparisons">
 				<span v-if="allComparisons.length == 1" class="identical-text">
 					{{allComparisons[0]}}
 				</span>
-				<span v-else :style="{backgroundColor: 'red'}">
-					{{allComparisons[0]}}
-				</span>
-			</span>
-		</div>	
-		<div class='col-6 mb-4'>
-				<span v-for="allComparisons in displayedTextArray['comparison']" v-bind:key="allComparisons">
-				<span v-if="allComparisons.length == 1" class="identical-text">
-					{{allComparisons[0]}}
-				</span>
-				<span v-else :style="{backgroundColor: 'red'}">
-					{{allComparisons[1]}}
+				<span v-else class="different-text">
+					{{allComparisons[i - 1]}}
 				</span>
 			</span>
 		</div>	
