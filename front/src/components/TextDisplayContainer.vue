88f1ec1951ffe2fce98ae2846f3c84b226f74b6d
Norman Kuusik <normankuusik@gmail.com>
2024-08-30 13:42:32 +0300

    Fix bug and add test.

--- front/src/components/TextDisplayContainer.vue
+++ front/src/components/TextDisplayContainer.vue
@@ -12,7 +12,6 @@ const isNull = (value) => {
 
 const isComparisonActive = ref(true)
 
-
 </script>
 
 <template>
@@ -24,18 +23,21 @@ const isComparisonActive = ref(true)
       			:label="`Highlight differences`">
 			</v-checkbox>
 		</div>
-		<div v-if="!isComparisonActive" v-for="text in displayedTextObject['lines']" v-bind:key="text" class='col-6 mb-4 displayed-texts'>
+		<div v-if="isComparisonActive && displayedTextObject['comparison'] !== undefined" >
+			<div v-for="i in 2" v-bind:key="i" class='col-6 mb-4 displayed-texts'>
+				<span v-for="allComparisons in displayedTextObject['comparison']" v-bind:key="allComparisons">
+					<span v-if="allComparisons.length == 1" class="identical-text">
+						{{allComparisons[0]}}
+					</span>
+					<span v-else class="different-text">
+						{{allComparisons[i - 1]}}
+					</span>
+				</span>
+			</div>
+		</div>	
+		<div v-else v-for="text in displayedTextObject['lines']" v-bind:key="text" class='col-6 mb-4 displayed-texts'>
 			{{ text }}
 		</div>
-		<div v-else v-for="i in 2" class='col-6 mb-4 displayed-texts'>
-				<span v-for="allComparisons in displayedTextObject['comparison']" v-bind:key="allComparisons">
-				<span v-if="allComparisons.length == 1" class="identical-text">
-					{{allComparisons[0]}}
-				</span>
-				<span v-else class="different-text">
-					{{allComparisons[i - 1]}}
-				</span>
-			</span>
-		</div>	
+		
 	</div>	
 </template>
