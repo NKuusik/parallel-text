82b039976da5e56c7d201516be7a888a93d16fb0
Norman Kuusik <normankuusik@gmail.com>
2024-09-03 10:51:58 +0300

    Add tests.

--- back/paralleltext/textparser/services/language_identifier.py
+++ back/paralleltext/textparser/services/language_identifier.py
@@ -12,9 +12,9 @@ class LanguageIdentifier:
         pass
 
     def identify_language(self, input_text):
-        language_identifier = gcld3.NNetLanguageIdentifier(min_num_bytes=0,                                                          max_num_bytes=1000000)        
+        language_identifier = gcld3.NNetLanguageIdentifier(min_num_bytes=0, max_num_bytes=1000000)        
         identified_language = language_identifier.FindLanguage(text=input_text)
 
-        if identified_language.probability > 0.9:
+        if identified_language.probability >= 0.99:
             return identified_language.language
         return 'und'
---
+++ back/paralleltext/textparser/tests/tests_services/test_parts_of_speech_tagger.py
@@ -0,0 +1,52 @@
+"""
+Test PartsOfSpeechTagger
+"""
+
+from django.test import TestCase
+import nltk
+from ...services.parts_of_speech_tagger import PartsOfSpeechTagger
+
+tagger = PartsOfSpeechTagger()
+
+class PartsOfSpeechTaggerTestCase(TestCase):
+    """
+    Tests for PartsOfSpeechTaggerTestCase service
+    
+    Note that we do not actually test language identification in these tests
+    """
+    def setUp(self):
+        """
+        Set up.
+        """
+
+    def tearDown(self):
+        """
+        Tear down.
+        """
+
+    def test_english_text_tags_correctly(self):
+        """
+        Input text in English is tagged correctly.
+        """
+
+        test_text_list = ["This is the first test text", "This is another test text"]
+        tagged_lines = tagger.tag_several_lines(test_text_list, 'en')
+
+        for result, test_text in zip(tagged_lines, test_text_list):
+            tokenized_words = nltk.word_tokenize(test_text)
+            self.assertEqual(result, nltk.pos_tag(tokenized_words))        
+
+    def test_other_languages_are_not_tagged(self):
+        """
+        Input text in any other language is not tagged.
+        """
+
+        test_text_list = ["See on mingis muus keeles."]
+        tagged_lines = tagger.tag_several_lines(test_text_list, 'et')
+        self.assertEqual(tagged_lines, None)
+
+        tagged_lines = tagger.tag_several_lines(test_text_list, 'ge')
+        self.assertEqual(tagged_lines, None)
+
+        tagged_lines = tagger.tag_several_lines(test_text_list, 'fr')
+        self.assertEqual(tagged_lines, None)
