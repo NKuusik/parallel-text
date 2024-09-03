8d50e036c4c746ae5c455ad3e0d3bf2d837cf72a
Norman Kuusik <normankuusik@gmail.com>
2024-09-03 15:28:47 +0300

    Manual reset to last working pull request.

--- back/paralleltext/textparser/apps.py
+++ back/paralleltext/textparser/apps.py
@@ -3,7 +3,6 @@ Apps
 """
 
 from django.apps import AppConfig
-import nltk
 
 class TextparserConfig(AppConfig):
     """
@@ -11,7 +10,3 @@ class TextparserConfig(AppConfig):
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'textparser'
-    def ready(self) -> None:
-        nltk.download('punkt')
-        nltk.download('averaged_perceptron_tagger_eng')
-        return super().ready()
